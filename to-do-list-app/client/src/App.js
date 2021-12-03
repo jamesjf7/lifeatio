c18f5dd33290545aa0b8d895137722c98f084596
jamesjf7 <jamesjeremyf2000@yahoo.com>
2021-12-04 03:02:22 +0800

    login register

--- to-do-list-app/client/src/App.js
+++ to-do-list-app/client/src/App.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Sw } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,8 +10,8 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} /> */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
